9f339f8e71950f63b7717aec95a65e6f4c15cb1f
lamchiomeng <lamchiomeng78@gmail.com>
2023-04-06 18:23:02 +0800

    Update Page_SplashScreen.java

--- Page_SplashScreen.java
+++ Page_SplashScreen.java
@@ -1,6 +1,5 @@
 import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
-
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -8,7 +7,6 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -16,14 +14,11 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.RemoteWebElement;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 public class Page_SplashScreen {
-	
 	//***** Define capabilities variable ***********
 	static AndroidDriver <MobileElement> driverApp;
 	static AppiumDriver <MobileElement> driverApp1;
-	static DesiredCapabilities dc = new DesiredCapabilities();
-	
+	static DesiredCapabilities dc = new DesiredCapabilities();	
 	public static void clickStartNow(DesiredCapabilities dc, AndroidDriver<MobileElement> driverApp) throws MalformedURLException {
 		System.out.println("Running Page_SplashScreen");
 		driverApp.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
